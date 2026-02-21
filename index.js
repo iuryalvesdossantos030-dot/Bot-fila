ccba10f1f9bda40db3902fdc7baa2af57ee80bc0
iuryalvesdossantos030-dot <iuryalvesdossantos030@gmail.com>
2026-02-21 19:57:11 -0300

    Refactor command execution and logging

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const fs = require('fs');
 const db = require('./database');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.commands = new Collection();
-
 const commandFiles = fs.readdirSync('./commands');
 
 for (const file of commandFiles) {
@@ -30,26 +29,9 @@ client.on('interactionCreate', async interaction => {
   const command = client.commands.get(interaction.commandName);
   if (!command) return;
 
-  try {
-    await command.execute(interaction, db, isOwner, isStaff);
-  } catch (error) {
-    console.error(error);
-  }
+  await command.execute(interaction, db, isOwner, isStaff);
+});
+console.log("Comandos registrados automaticamente!");
 });
 
-client.once('ready', async () => {
-  console.log(`Logado como ${client.user.tag}`);
-
-  const commands = [];
-  const commandFiles = fs.readdirSync('./commands');
-
-  for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-  }
-
-  await client.guilds.cache.get(process.env.GUILD_ID)?.commands.set(commands);
-
-  console.log("Comandos registrados automaticamente!");
-});
 client.login(process.env.TOKEN);
