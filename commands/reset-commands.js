bc2642d26575f342c069a3761b78d4a7f2d91e75
iuryalvesdossantos030-dot <iuryalvesdossantos030@gmail.com>
2026-02-21 20:50:33 -0300

    Create reset-commands.js

---
+++ commands/reset-commands.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('🗑 Limpando comandos antigos...');
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: [] }
+    );
+
+    console.log('✅ Comandos apagados!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
