45a1dbdaee448fd4c13db489feecbdde7781d89d
iuryalvesdossantos030-dot <iuryalvesdossantos030@gmail.com>
2026-02-21 22:21:37 -0300

    Update database.js

--- database.js
+++ database.js
@@ -3,12 +3,26 @@ const fs = require('fs');
 const file = './data.json';
 
 function read() {
-  if (!fs.existsSync(file)) fs.writeFileSync(file, JSON.stringify({}));
+  if (!fs.existsSync(file)) {
+    fs.writeFileSync(file, JSON.stringify({
+      pix: null,
+      filas: {},
+      ranking: {}
+    }));
+  }
   return JSON.parse(fs.readFileSync(file));
 }
 
 function write(data) {
   fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+function get() {
+  return read();
+}
+
+function set(data) {
+  write(data);
 }
 
 function setPix(key) {
@@ -23,6 +37,8 @@ function getPix() {
 }
 
 module.exports = {
+  get,
+  set,
   setPix,
   getPix
 };
