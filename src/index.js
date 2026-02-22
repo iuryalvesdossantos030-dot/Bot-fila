04cc67bd1ac30b11c0a4f0984b5c1b63b3b3e88b
iuryalvesdossantos030-dot <iuryalvesdossantos030@gmail.com>
2026-02-22 01:58:49 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require("discord.js")
+const config = require("./config.json")
+const painelPrincipal = require("./panels/painelPrincipal")
+const buttons = require("./interactions/buttons")
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
+})
+
+buttons(client)
+
+client.on("interactionCreate", async interaction => {
+  if (interaction.isChatInputCommand()) {
+    if (interaction.commandName === "painel")
+      painelPrincipal(interaction)
+  }
+})
+console.log("Comandos registrados automaticamente!");
+});
+client.login(config.token)
