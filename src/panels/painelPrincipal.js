afc1436bba7fdb5b3f2e4ec9b1a5bba64417661b
iuryalvesdossantos030-dot <iuryalvesdossantos030@gmail.com>
2026-02-22 18:19:35 -0300

    Update painelPrincipal.js

--- src/panels/painelPrincipal.js
+++ src/panels/painelPrincipal.js
@@ -1,3 +1,21 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
 module.exports = async (interaction) => {
-  await interaction.reply("Teste");
+
+  const embed = new EmbedBuilder()
+    .setColor("#2b2d31")
+    .setTitle("⚙️ Black Config");
+
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId("gel_normal")
+      .setLabel("Filas 1v1")
+      .setStyle(ButtonStyle.Primary)
+  );
+
+  await interaction.reply({
+    embeds: [embed],
+    components: [row]
+  });
+
 };
