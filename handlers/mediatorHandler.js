c5c7d6c9365331c0a8ee129efe5b27b57aabac9c
iuryalvesdossantos030-dot <iuryalvesdossantos030@gmail.com>
2026-02-23 00:03:13 -0300

    Update mediatorHandler.js

--- handlers/mediatorHandler.js
+++ handlers/mediatorHandler.js
@@ -1,16 +1,10 @@
-import redis from '../services/redis.js';
+import redis from '../utils/redis.js';
 
-export async function joinMediator(interaction) {
-  const list = await redis.lrange('mediators', 0, -1);
-  if (list.includes(interaction.user.id)) {
-    return interaction.reply({ content: '⚠️ Você já está na fila.', ephemeral: true });
-  }
+export async function entrarMediador(interaction) {
+  await redis.sadd('mediadores', interaction.user.id);
 
-  await redis.rpush('mediators', interaction.user.id);
-  await interaction.reply({ content: '⚖️ Entrou na fila de mediadores.', ephemeral: true });
-}
-
-export async function leaveMediator(interaction) {
-  await redis.lrem('mediators', 0, interaction.user.id);
-  await interaction.reply({ content: '❌ Saiu da fila de mediadores.', ephemeral: true });
+  await interaction.reply({
+    content: '⚖️ Você entrou na fila de mediadores.',
+    ephemeral: true
+  });
 }
